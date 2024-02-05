0988801c7146205a735e45d35b6f800eb8f91342
Rahul Rawal <rahulrawal3101@gmail.com>
2024-02-05 18:17:06 +0530

    adding new compo

--- src/componant/Header.js
+++ src/componant/Header.js
@@ -1,38 +1,56 @@
 'use client'
-import {Box, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 // import Image from 'next/image';
 import React from 'react';
 // import teslalogo from '../assets/teslalogo.png';
 import teslwhitelogo from '../assets/teslawhitelogo.png';
 import mainlogo from '../assets/mainlogo.png';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 const Header = () => {
     return (
         <>
-            <Grid container sx={{position:'sticky', top:'0px', zIndex:99}}>
+            <Grid container sx={{ position: 'sticky', top: '0px', zIndex: 99 }}>
                 <Grid container sx={{ justifyContent: 'center', alignItems: 'center', background: 'linear-gradient(94deg, rgba(0,0,0,1) 0%, rgba(57,55,55,1) 31%, rgba(0,0,3,1) 46%)', p: '12px' }}>
-                    <Grid item xs={11} sx={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center' }}>
-                        <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: '18px',  }}>$7,500 Federal Tax Credit</Typography>
-                        <Typography sx={{ fontSize: '14px', color: 'white', fontWeight: 'bold', letterSpacing:'1px' }}>Eligible buyers now receive $7,500 off the purchase price of new 2024 Model Y and Model X Dual Motor. Applied at time of delivery.</Typography>
-                        <Typography sx={{ fontSize: '17px', color: 'white' }}>See Details</Typography>
+                    <Grid item lg={2} md={2.5} sm={12} xs={12} >
+                        <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: { lg: '17px', md: '16px', sm: '16px', xs: '15px' }, textAlign: { lg: 'center', md: 'center', sm: 'left', xs: 'left' } }}>$7,500 Federal Tax Credit</Typography>
+
+                    </Grid>
+                    <Grid item lg={8.2} md={7.5} sm={12} xs={12} >
+
+                        <Typography sx={{ fontSize: { lg: '14px', md: '13px', sm: '12px', xs: '11px' }, color: 'white', fontWeight: 'bold', letterSpacing: '1px', textAlign: { lg: 'center', md: 'center', sm: 'left', xs: 'left' }, mt: { lg: '0px', md: '0px', sm: '5px', xs: '5px',  } }}>Eligible buyers now receive $7,500 off the purchase price of new 2024 Model Y and Model X Dual Motor. Applied at time of delivery.</Typography>
+                    </Grid>
+                    <Grid item lg={1} md={1.4} sm={12} xs={12}>
+                        <Typography sx={{ fontSize: { lg: '17px', md: '15px', sm: '15px', xs: '15px' }, textAlign: { lg: 'center', md: 'center', sm: 'left', xs: 'left' }, mt: { lg: '0px', md: '0px', sm: '4px', xs: '4px' } }}><a href='' style={{ color: 'white' }}>See Details</a></Typography>
 
                     </Grid>
                 </Grid>
-                <Grid container sx={{  alignItems:'center', bgcolor:'transparent', zIndex:9999,}}>
-                    <Grid item xs={4.5} >
-                        <Box sx={{width:'130px', height:'60px', ml:'30px', }}>
+                <Grid container sx={{ justifyContent: 'space-between', alignItems: 'center', bgcolor: 'transparent', zIndex: 9999, }}>
+                    <Grid item xs={3} >
+                        <Box sx={{ width: { lg: '130px', md: '130px', sm: '100px', xs: '82px' }, height: '60px', ml: '30px', }}>
 
-                        <img src={mainlogo} style={{width:'100%', height:'100%'}}/>
+                            <img src={mainlogo} style={{ width: '100%', height: '100%' }} />
                         </Box>
                     </Grid>
 
-                    <Grid item xs={3.3} sx={{ display:'flex', justifyContent:'space-between', alignItems:'flex-end'}}>
-                        <Typography sx={{fontSize:'15px', color:'#212121', fontWeight:'bold', color:'white'}}>Vehicles</Typography>
-                        <Typography sx={{fontSize:'15px', color:'#212121',fontWeight:'bold', color:'white' }}>Energy</Typography>
-                        <Typography sx={{fontSize:'15px', color:'#212121',fontWeight:'bold' , color:'white'}}>Charging</Typography>
-                        <Typography sx={{fontSize:'15px', color:'#212121',fontWeight:'bold', color:'white' }}>Discover</Typography>
-                        <Typography sx={{fontSize:'15px', color:'#212121', fontWeight:'bold', color:'white'}}>Shop</Typography>
+                    <Grid item xs={3.3} sx={{ display: { lg: 'flex', mf: 'flex', sm: 'none', xs: 'none' }, justifyContent: 'space-between', alignItems: 'flex-end' }}>
+                        <Typography sx={{ fontSize: '15px', color: '#212121', fontWeight: 'bold', color: 'white' }}>Vehicles</Typography>
+                        <Typography sx={{ fontSize: '15px', color: '#212121', fontWeight: 'bold', color: 'white' }}>Energy</Typography>
+                        <Typography sx={{ fontSize: '15px', color: '#212121', fontWeight: 'bold', color: 'white' }}>Charging</Typography>
+                        <Typography sx={{ fontSize: '15px', color: '#212121', fontWeight: 'bold', color: 'white' }}>Discover</Typography>
+                        <Typography sx={{ fontSize: '15px', color: '#212121', fontWeight: 'bold', color: 'white' }}>Shop</Typography>
 
+                    </Grid>
+                    <Grid item xs={3} sx={{ display:{lg: 'flex', md:'flex', sm:'none', xs:'none'}, justifyContent: 'right', alignItems: 'center', p: '0px 15px' }}>
+                        <HelpOutlineOutlinedIcon sx={{ color: 'white', fontSize: '25px', mr: '7px' }} />
+                        <LanguageOutlinedIcon sx={{ color: 'white', fontSize: '25px', mr: '7px' }} />
+                        <PersonOutlineOutlinedIcon sx={{ color: 'white', fontSize: '25px' }} />
+
+                    </Grid>
+                    <Grid item xs={3} sx={{display:{lg: 'none', md:'none', sm:'flex', xs:'flex'}, justifyContent: 'right', alignItems: 'center', p: '0px 15px' }}>
+                        <Button sx={{ bgcolor: '#e0e0e0', color: 'white', textTransform: 'capitalize', background: 'rgba(0,0,0,0.5)' }}>Menu</Button>
                     </Grid>
 
                 </Grid>
--- src/componant/MpSection1.js
+++ src/componant/MpSection1.js
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import React, { useEffect } from 'react';
 import ReactPlayer from 'react-player';
 import video from '../assets/property.mp4';
@@ -11,6 +11,9 @@ import office from '../assets/office.webp';
 import bgimg from '../assets/bgimg.jpg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import img4 from '../assets/img4.png';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
 
 
 const listArr = [
@@ -48,30 +51,31 @@ const listArr = [
 
 const MpSection1 = () => {
     useEffect(() => {
-        AOS.init({ duration: 2000});
+        AOS.init({ duration: 2000 });
     }, []);
     return (
         <>
 
-            <Grid container sx={{ mt: '-154px', position: 'relative', }}>
-                <Grid item xs={12}>
-                    <ReactPlayer url={`${video}`} width={'100%'} height={'100%'} controls={false} playing={true} loop={true} muted={true} style={{border:'none'}} />
-                    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.5)', }} />
+            <Grid container sx={{ mt: {lg:'-154px', md:'-154px', sm:'-100px', xs:'-65px'}, position: 'relative' }}>
+                <Grid item xs={12} >
+                    <ReactPlayer url={`${video}`} width={'100%'} height={'100%'} controls={false} playing={true} loop={true} muted={true} style={{ border: 'none' }} />
+                    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0, 0, 0, 0.3)', }} />
 
-                    <Grid container sx={{ position: 'absolute', top: '170px', }}>
+                    <Grid container sx={{ position: 'absolute', top: { lg: '170px', md: '110px', sm: '70px', xs: '30px' }, }}>
                         <Grid item xs={12}>
-                            <Typography sx={{ fontSize: '50px', color: 'white', textAlign: 'center' }}>Golden Earth</Typography>
-                            <Typography sx={{ color: 'white', fontSize: '25px', textAlign: 'center' }}>Lease starting at $329/mo*</Typography>
+                            <Typography sx={{ fontSize: { lg: '50px', md: '40px', sm: '30px', xs: '18px' }, color: 'white', textAlign: 'center' }}>Golden Earth</Typography>
+                            <Typography sx={{ color: 'white', fontSize: { lg: '25px', md: '20px', sm: '17px', xs: '14px' }, textAlign: 'center' }}>Lease starting at $329/mo*</Typography>
 
                         </Grid>
                     </Grid>
-                    <Grid container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', position: 'absolute', bottom: '120px', zIndex: 1 }}>
-                        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', pb: '27px' }}>
-                            <Button variant='contained' sx={{ bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', fontSize: '12px', width: '265px', fontWeight: 'bold', mr: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', } }}>Custom Order</Button>
-                            <Button variant='contained' sx={{ bgcolor: 'rgba(23, 26, 32, 0.65)', color: 'white', fontSize: '12px', width: '265px', fontWeight: 'bold', ml: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(23, 26, 32, 0.65)', color: '#424242', } }}>Learn More</Button>
+                    <Grid container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', position: 'absolute', bottom: { lg: '120px', md: '50px', sm: '1px', xs: '0px' }, zIndex: 1 }}>
+                        <Grid item xs={12} sx={{ display: { lg: 'flex', md: 'flex', sm: 'flex', xs: 'flex' }, justifyContent: 'center', alignItems: 'center', pb: '27px' }}>
+                            <Button variant='contained' sx={{ bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', fontSize: { lg: '12px', md: '11px', sm: '10px', xs: '10px' }, width: { lg: '265px', md: '200px', sm: '110px', xs: '120px' }, fontWeight: 'bold', mr: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', } }}>Custom Order</Button>
+                            <Button variant='contained' sx={{ bgcolor: 'rgba(23, 26, 32, 0.65)', color: 'white', fontSize: { lg: '12px', md: '11px', sm: '10px', xs: '10px' }, width: { lg: '265px', md: '200px', sm: '110px', xs: '120px' }, fontWeight: 'bold', ml: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(23, 26, 32, 0.65)', color: '#424242', } }}>Learn More</Button>
 
                         </Grid>
-                        <Grid item xs={12} sx={{ pb: '17px' }} >
+
+                        <Grid item xs={12} sx={{ pb: '17px', display: { lg: 'block', md: 'block', sm: 'none', xs: 'none' } }} >
                             <Typography sx={{ color: 'white', fontSize: '13px', textAlign: 'center' }}>*Excludes taxes and fees with price subject to change. Available in select states. See Details</Typography>
                         </Grid>
 
@@ -79,17 +83,17 @@ const MpSection1 = () => {
                 </Grid>
             </Grid>
 
-            <Grid container sx={{  display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', backgroundImage: `url(${bgimg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundColor: 'rgba(150,150,150)', backgroundBlendMode: 'multiply' , p:'30px'}}>
-                <Grid item xs={12} sx={{pb:'15px'}}>
-                    <Typography sx={{ color: '#eeeeee', fontSize: '30px', fontWeight: 'bold', textAlign: 'center' }}>All Types Of Property</Typography>
+            <Grid container sx={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', backgroundImage: `url(${bgimg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundColor: 'rgba(0,0,0,0.5)', backgroundBlendMode: 'multiply', p: '30px', mt: '-5px' }}>
+                <Grid item xs={12} sx={{ pb: '15px', }}>
+                    <Typography sx={{ color: '#eeeeee', fontSize: { lg: '50px', md: '40px', sm: '30px', xs: '18px' }, textAlign: 'center', mt: '17px' }}>All Types Of Property</Typography>
 
                 </Grid>
                 {
                     listArr.map((ele) => {
                         return (
-                            <Grid item  xs={3.1} sx={{ height: '260px', backgroundImage: `url(${ele.image})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundColor: 'rgba(230,230,230)', backgroundBlendMode: 'multiply', borderRadius: '7px', display:'flex',justifyContent:'space-between', alignItems:'flex-end', m:'10px', boxShadow:'1px 1px 2px 0px rgba(252,249,251,1)', p:'5px'}} data-aos="fade-up">
-                                <Typography sx={{fontSize:'22px', color:'#e0e0e0', fontWeight:'bold'}}>{ele.name}</Typography>
-                                <Typography sx={{fontSize:'20px', color:'white'}}>{ele.type}</Typography>
+                            <Grid item lg={3.1} md={4} sm={5} xs={12} data-aos="fade-up" sx={{ height: { lg: '260px', md: '230px', sm: '190px', xs: '190px' }, backgroundImage: `url(${ele.image})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundColor: 'rgba(0,0,0,0.5)', backgroundBlendMode: 'multiply', borderRadius: '7px', display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end', m: '10px', boxShadow: '1px 1px 2px 0px rgba(252,249,251,1)', p: '5px' }} >
+                                <Typography sx={{ fontSize: { lg: '22px', md: '20px', sm: '17px', xs: '15px' }, color: '#e0e0e0', fontWeight: 'bold' }}>{ele.name}</Typography>
+                                <Typography sx={{ fontSize: { lg: '20px', md: '18px', sm: '16px', xs: '14px' }, color: 'white' }}>{ele.type}</Typography>
 
                             </Grid>
                         )
@@ -99,11 +103,75 @@ const MpSection1 = () => {
 
             </Grid>
 
-            {/* <Grid container>
-                <Grid item xs={12} sx={{bgcolor:'black', p:'20px'}}>
-                    <Typography sx={{fontSize:'30px', color:'white', fontWeight:'bold', textAlign:'center'}}>Contact Us</Typography>
-                </Grid>
-            </Grid> */}
+            <Grid container sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column', height: { lg: '700px', md: '500px', sm: '400px', xs:'320px' }, backgroundImage: `url(${img4})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', backgroundColor: 'rgba(150,150,150)', backgroundBlendMode: 'multiply', p: '30px' }}>
+                <Box >
+                    <Typography sx={{ fontSize: { lg: '50px', md: '40px', sm: '30px', xs: '18px' }, color: 'white', textAlign: 'center' }}>Golden Earth</Typography>
+                    <Typography sx={{ color: 'white', fontSize: { lg: '25px', md: '20px', sm: '17px', xs: '14px' }, textAlign: 'center' }}>Lease starting at $329/mo*</Typography>
+
+                </Box>
+                <Box>
+                    <Grid container sx={{ justifyContent: 'center', alignItems: 'center', }}>
+                        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: {lg:'30px', md:'20px', sm:'10px', xs:'0px'} }}>
+                            <Button variant='contained' sx={{ bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', fontSize: { lg: '12px', md: '11px', sm: '10px', xs: '10px' }, width: { lg: '265px', md: '200px', sm: '110px', xs: '120px' }, fontWeight: 'bold', mr: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', } }}>Custom Order</Button>
+                            <Button variant='contained' sx={{ bgcolor: 'rgba(23, 26, 32, 0.65)', color: 'white', fontSize: { lg: '12px', md: '11px', sm: '10px', xs: '10px' }, width: { lg: '265px', md: '200px', sm: '110px', xs: '120px' }, fontWeight: 'bold', ml: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(23, 26, 32, 0.65)', color: '#424242', } }}>Learn More</Button>
+
+                        </Grid>
+                        <Grid item xs={12} sx={{ display: { lg: 'flex', md: 'flex', sm: 'none', xs: 'none' }, justifyContent: 'center', alignItems: 'flex-end', }} >
+                            <Typography sx={{ color: 'white', fontSize: '13px', textAlign: 'center' }}>*Excludes taxes and fees with price subject to change. Available in select states. See Details</Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+
+
+
+
+            </Grid>
+            <Grid container sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column', height: { lg: '700px', md: '500px', sm: '400px', xs:'320px' }, backgroundImage: `url(${img2})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', backgroundColor: 'rgba(150,150,150)', backgroundBlendMode: 'multiply', p: '30px' }}>
+                <Box >
+                    <Typography sx={{ fontSize: { lg: '50px', md: '40px', sm: '30px', xs: '18px' }, color: 'white', textAlign: 'center' }}>Golden Earth</Typography>
+                    <Typography sx={{ color: 'white', fontSize: { lg: '25px', md: '20px', sm: '17px', xs: '14px' }, textAlign: 'center' }}>Lease starting at $329/mo*</Typography>
+
+                </Box>
+                <Box>
+                    <Grid container sx={{ justifyContent: 'center', alignItems: 'center', }}>
+                        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: {lg:'30px', md:'20px', sm:'10px', xs:'0px'} }}>
+                            <Button variant='contained' sx={{ bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', fontSize: { lg: '12px', md: '11px', sm: '10px', xs: '10px' }, width: { lg: '265px', md: '200px', sm: '110px', xs: '120px' }, fontWeight: 'bold', mr: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', } }}>Custom Order</Button>
+                            <Button variant='contained' sx={{ bgcolor: 'rgba(23, 26, 32, 0.65)', color: 'white', fontSize: { lg: '12px', md: '11px', sm: '10px', xs: '10px' }, width: { lg: '265px', md: '200px', sm: '110px', xs: '120px' }, fontWeight: 'bold', ml: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(23, 26, 32, 0.65)', color: '#424242', } }}>Learn More</Button>
+
+                        </Grid>
+                        <Grid item xs={12} sx={{ display: { lg: 'flex', md: 'flex', sm: 'none', xs: 'none' }, justifyContent: 'center', alignItems: 'flex-end', }} >
+                            <Typography sx={{ color: 'white', fontSize: '13px', textAlign: 'center' }}>*Excludes taxes and fees with price subject to change. Available in select states. See Details</Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+
+
+
+
+            </Grid>
+            <Grid container sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column', height: { lg: '700px', md: '500px', sm: '400px', xs:'320px' }, backgroundImage: `url(${img3})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', backgroundColor: 'rgba(150,150,150)', backgroundBlendMode: 'multiply', p: '30px' }}>
+                <Box >
+                    <Typography sx={{ fontSize: { lg: '50px', md: '40px', sm: '30px', xs: '18px' }, color: 'white', textAlign: 'center' }}>Golden Earth</Typography>
+                    <Typography sx={{ color: 'white', fontSize: { lg: '25px', md: '20px', sm: '17px', xs: '14px' }, textAlign: 'center' }}>Lease starting at $329/mo*</Typography>
+
+                </Box>
+                <Box>
+                    <Grid container sx={{ justifyContent: 'center', alignItems: 'center', }}>
+                        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: {lg:'30px', md:'20px', sm:'10px', xs:'0px'} }}>
+                            <Button variant='contained' sx={{ bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', fontSize: { lg: '12px', md: '11px', sm: '10px', xs: '10px' }, width: { lg: '265px', md: '200px', sm: '110px', xs: '120px' }, fontWeight: 'bold', mr: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(244, 244, 244, 0.8)', color: '#424242', } }}>Custom Order</Button>
+                            <Button variant='contained' sx={{ bgcolor: 'rgba(23, 26, 32, 0.65)', color: 'white', fontSize: { lg: '12px', md: '11px', sm: '10px', xs: '10px' }, width: { lg: '265px', md: '200px', sm: '110px', xs: '120px' }, fontWeight: 'bold', ml: '15px', p: '10px', '&:hover': { bgcolor: 'rgba(23, 26, 32, 0.65)', color: '#424242', } }}>Learn More</Button>
+
+                        </Grid>
+                        <Grid item xs={12} sx={{ display: { lg: 'flex', md: 'flex', sm: 'none', xs: 'none' }, justifyContent: 'center', alignItems: 'flex-end', }} >
+                            <Typography sx={{ color: 'white', fontSize: '13px', textAlign: 'center' }}>*Excludes taxes and fees with price subject to change. Available in select states. See Details</Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+
+
+
+
+            </Grid>
         </>
     );
 }
